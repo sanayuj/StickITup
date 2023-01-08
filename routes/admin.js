56e09964977076a5d5699235d5e08b75ecf9ea70
Sanay UJ <admin@Sanay-MacBook.local>
2023-01-08 20:59:35 +0530

    updates

--- routes/admin.js
+++ routes/admin.js
@@ -1,65 +1,67 @@
-const adminController=require('../control/admin_control')
+const adminController = require('../control/admin_control')
 var express = require('express');
 const { response } = require('../app');
 var router = express.Router();
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('adminPage/login');
 });
 
-router.get('/admin_homepage',function(req,res,next){
+router.get('/admin_homepage', function (req, res, next) {
   res.render('adminPage/admin_dash')
 })
 
-router.get('/admin_userlist',function(req,res){
-  adminController.getuserData().then((response)=>{
+router.get('/admin_userlist', function (req, res) {
+  adminController.getuserData().then((response) => {
     //console.log(response)
-    if(response.status){
-      res.render('adminPage/admin_userlist',{userdetails:response.userdata})
-    }else{
+    if (response.status) {
+      res.render('adminPage/admin_userlist', { userdetails: response.userdata })
+    } else {
       res.send(error)
     }
   })
-  
+
 })
 
-router.post('/admin_login',function(req,res,next){
+router.post('/admin_login', function (req, res, next) {
   //console.log(req.body)
-adminController.doadminloggin(req.body).then((response)=>{
-  if(response.adminNotExist){
-    req.session.adminIDnotExist=true
-    console.log(req.session.adminIDnotExist)
-    res.redirect('/admin/')
-  }else{
-    if(response.status){
-      req.session.adminloggedin=true
-      req.session.user=response.adminDetails
-   
-       res.redirect('/admin/admin_homepage')
-     }else{
-      // req.session.adminpasswordErr=true
-       res.redirect('/admin/')
-     }
-    
-  }
-  
-}),
-router.get('/admin_logout',(req,res)=>{
-  res.redirect('/admin/')
-  // req.session.destroy()
+  adminController.doadminloggin(req.body).then((response) => {
+    if (response.adminNotExist) {
+      req.session.adminIDnotExist = true
+      console.log(req.session.adminIDnotExist)
+      res.redirect('/admin/')
+    } else {
+      if (response.status) {
+        req.session.adminloggedin = true
+        req.session.user = response.adminDetails
+
+        res.redirect('/admin/admin_homepage')
+      } else {
+        // req.session.adminpasswordErr=true
+        res.redirect('/admin/')
+      }
+
+    }
+
+  })
 })
+  ,
+  router.get('/admin_logout', (req, res) => {
+    res.redirect('/admin/')
+    // req.session.destroy()
+  })
 
 
-router.get('/block_user',(req,res)=>{
+router.get('/block_user/:id', (req, res) => {
   console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-  adminController.blockUser(req.params.id).then((response)=>{
+  adminController.blockUser(req.params.id).then((response) => {
     console.log(response.userId)
+    console.log(req.params.id)
     res.redirect('/admin/admin_userlist')
   })
 })
 
-})
 
 module.exports = router;
