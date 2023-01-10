271a02478dc53cf35565223934c8f7c55ac8ce8c
Sanay UJ <admin@Sanay-MacBook.local>
2023-01-10 13:15:32 +0530

    category

---
+++ model/categorymodel.js
@@ -0,0 +1,10 @@
+const mongoose=require("mongoose")
+
+const categorySchema= new mongoose.Schema({
+    title:{
+        type:String,
+        require:true
+    }
+})
+
+module.exports=new mongoose.model("categories",categorySchema)
