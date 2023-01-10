d431f08cfd3aca15dcdca1433603dcead5e77361
Sanay UJ <admin@Sanay-MacBook.local>
2023-01-10 12:14:32 +0530

    product scheme

---
+++ model/productmodel.js
@@ -0,0 +1,34 @@
+const mongoose=require('mongoose')
+
+const productSchema= new mongoose.Schema({
+    name:{
+        type:String ,
+        require:true   
+    },
+    price:{
+        type:Number,
+        require:true
+    },
+    product_description:{
+        type:String,
+        require:true
+    },
+    category:{
+        type:String,
+        require:true
+    },
+    size:{
+        type:String,
+        require:true
+    },
+    images:[{
+        type:String,
+        require:true
+    }],
+    stock:{
+        type:Number,
+        require:true
+    }
+
+})
+module.exports=new mongoose.module("products",productSchema)
